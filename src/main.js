e6fd84e01f4a9a371c81d0cf758d0e11855613a8
Chris Annunziato <cmannunziato@gmail.com>
2025-04-04 21:07:07 -0400

    added cell data object

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 const app = document.querySelector('#app')
 
+
 app.innerHTML = `
 
 <div class="bg-gray-400 w-screen h-screen flex justify-center items-center px-10">
@@ -11,15 +12,27 @@ app.innerHTML = `
 </div>
 
  `
+const createCell = (rowIndex, cellIndex) => {
+    return {
+        index: rowIndex * 30 + cellIndex,
+        row: rowIndex,
+        cell: cellIndex,
+        isAlive: false,
+    }
+}
+
+
+const arr = Array.from({length: 30}, (_, rowIndex) =>
+    Array.from({length: 30}, (_,cellIndex) => createCell(rowIndex, cellIndex)))
+
 const primary = document.querySelector('.primary')
-
-const arr = Array.from({length: 30}, (_, rowIndex) => Array.from({length: 30}, (colIndex) => {
-}))
-
 arr.forEach((row) => {
-    row.forEach(() => {
+    row.forEach((cell) => {
         const div = document.createElement('div')
         div.className = 'h-3 w-3 border bg-green-500'
+        div.dataset.index = cell.index
+        div.dataset.row = cell.row
+        div.dataset.cell = cell.cell
         primary.appendChild(div)
     })
 })
