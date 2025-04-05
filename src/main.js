66623cd2ca672304c64c757aa8c2252eb37ec29e
Chris Annunziato <cmannunziato@gmail.com>
2025-04-04 20:16:16 -0400

    added grid of cells

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div class="h-screen w-screen bg-gray-500 flex justify-center items-center">
-  <div>hey</div>
- </div>
-`
+const app = document.querySelector('#app')
+
+app.innerHTML = `
+
+<div class="bg-gray-400 w-screen h-screen flex justify-center items-center px-10">
+<div class="primary flex flex-wrap w-[720px]"> 
+
+</div>
+</div>
+
+ `
+const primary = document.querySelector('.primary')
+
+const arr = Array.from({length: 30}, (_, rowIndex) => Array.from({length: 30}, (colIndex) => {
+}))
+
+arr.forEach((row) => {
+    row.forEach(() => {
+        const div = document.createElement('div')
+        div.className = 'h-3 w-3 border bg-green-500'
+        primary.appendChild(div)
+    })
+})
+
+console.log(app)
 
