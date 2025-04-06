ed8f8e466e9ff222ef66cdfe9e561bbeddb763b0
Chris Annunziato <cmannunziato@gmail.com>
2025-04-06 18:10:06 -0400

    fixed grid to properly display index and removed the need for flex wrap

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const app = document.querySelector('#app')
 app.innerHTML = `
 
 <div class="bg-gray-400 w-screen h-screen flex justify-center items-center px-10">
-<div class="primary flex flex-wrap w-[720px]"> 
+<div class="grid flex flex-col"> 
 
 </div>
 </div>
@@ -14,26 +14,29 @@ app.innerHTML = `
  `
 const createCell = (rowIndex, cellIndex) => {
     return {
-        index: rowIndex * 30 + cellIndex,
+        index: rowIndex * 60 + cellIndex,
         row: rowIndex,
         cell: cellIndex,
         isAlive: false,
     }
 }
 
+const arr = Array.from({length: 15}, (_, rowIndex) =>
+    Array.from({length: 60}, (_, cellIndex) => createCell(rowIndex, cellIndex)))
 
-const arr = Array.from({length: 30}, (_, rowIndex) =>
-    Array.from({length: 30}, (_,cellIndex) => createCell(rowIndex, cellIndex)))
-
-const primary = document.querySelector('.primary')
+const grid = document.querySelector('.grid')
 arr.forEach((row) => {
+    const rowDiv = document.createElement('div')
+    rowDiv.className = 'rowDiv flex'
+    grid.appendChild(rowDiv)
+
     row.forEach((cell) => {
-        const div = document.createElement('div')
-        div.className = 'h-3 w-3 border bg-green-500'
-        div.dataset.index = cell.index
-        div.dataset.row = cell.row
-        div.dataset.cell = cell.cell
-        primary.appendChild(div)
+        const cellDiv = document.createElement('div')
+        cellDiv.className = 'h-3 w-3 border bg-green-500'
+        cellDiv.dataset.index = cell.index
+        cellDiv.dataset.row = cell.row
+        cellDiv.dataset.cell = cell.cell
+        rowDiv.appendChild(cellDiv)
     })
 })
 
