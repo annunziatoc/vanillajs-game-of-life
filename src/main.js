28878013f0871d933b41cfa07c539a004b91e630
Chris Annunziato <cmannunziato@gmail.com>
2025-04-18 10:54:10 -0400

    new working copy

--- src/main.js
+++ src/main.js
@@ -2,97 +2,51 @@ import './style.css'
 
 const app = document.querySelector('#app')
 
-
 app.innerHTML = `
+<div class="grid-container"> </div>
+`
+const grid = document.querySelector('.grid-container')
+// MODEL
 
-<div class="bg-gray-400 w-screen h-screen flex justify-center items-center px-10">
-<div class="grid flex flex-col"> 
+//model of cell state in an object
+const dataModel = (rowIndex, cellIndex) => ({
+    rowIndex: rowIndex, cellIndex: cellIndex, isAlive: false
+})
 
-</div>
-</div>
-
- `
-
-//DATA MODEL
-const createCell = (rowIndex, cellIndex) => {
-    return {
-        index: rowIndex * 60 + cellIndex, row: rowIndex, cell: cellIndex, isAlive: false,
-    }
-}
-
-//track what indices are alive
-let currentAlive = []
-
-
-//gen an array in memory
-
-let arr = Array.from({length: 15}, (_, rowIndex) => Array.from({length: 60}, (_, cellIndex) => createCell(rowIndex, cellIndex)))
-
-
+//array in memory
+const arr = Array.from({length: 15}, (_, rowIndex) => Array.from({length: 60}, (_, cellIndex) => dataModel(rowIndex, cellIndex)))
 
 //VIEW
-//fill the array with divs
-//each grid has rows
-const grid = document.querySelector('.grid')
-arr.forEach((row, rowIndex) => {
-    const rowDiv = document.createElement('div')
-    rowDiv.className = 'rowDiv flex'
-    rowDiv.dataset.index = rowIndex.toString()
-    grid.appendChild(rowDiv)
 
-//each row is comprised of cells
-// link model and views
-    row.forEach((cell) => {
+const isAliveArray = []
+//gen a random alive cell
+function generateAliveGridCell () {
+    const randRowInt = Math.floor(Math.random() * 15)
+    const randCellInt = Math.floor(Math.random() * 60)
+    isAliveArray.push([randRowInt, randCellInt])
+    return `${randRowInt}-${randCellInt}`
+}
+
+//create divs, rows and cells
+//decorate them and append them to each other creating a 2d matrix
+arr.forEach((row, rowIdx) => {
+    const rowContainer = document.createElement('div')
+    rowContainer.className = (`row-container flex`)
+
+    grid.appendChild(rowContainer)
+
+    row.forEach((cell, cellIdx) => {
         const cellDiv = document.createElement('div')
-        cellDiv.className = `h-3 w-3 border bg-teal-500`
-        cellDiv.dataset.index = cell.index
-        cellDiv.dataset.row = cell.row
-        cellDiv.dataset.cell = cell.cell
-        cellDiv.dataset.isAlive = cell.isAlive
-        rowDiv.appendChild(cellDiv)
+        cellDiv.className = (`cell border h-4 w-4 ${cell.isAlive ? 'bg-green-500' : ''}`)
+        cellDiv.dataset.id = `${rowIdx}-${cellIdx}`
+
+        //append alive cell to the dom
+        const randInt = generateAliveGridCell()
+        cellDiv
+
+        rowContainer.appendChild(cellDiv)
     })
 })
 
-function genInitialBoard() {
+console.log(grid)
 
-    function randInt() {
-        return Math.floor(Math.random() * 899)
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const randCell = document.querySelector(`[data-index="${randInt().toString()}"]`)
-        randCell.classList.remove('bg-teal-500')
-        randCell.classList.add('bg-red-500')
-        randCell.dataset.isAlive = true.toString()
-        currentAlive.push(randCell.getAttribute('data-index'))
-
-    }
-}
-
-genInitialBoard()
-
-function addMask (currentAlive) {
-    console.log("Current alive cells", currentAlive)
-
-    currentAlive.forEach((aliveIndex) => {
-        const nextIndex = Number(aliveIndex) + 1
-        console.log(`Looking for the cell with data-index ${nextIndex}`)
-        const nextCell = document.querySelector(`[data-index="${nextIndex}"]`)
-
-        if(nextCell) {
-            nextCell.dataset.isAlive = true.toString()
-            nextCell.classList.remove('bg-teal-500')
-            nextCell.classList.add('bg-red-500')
-        }
-
-    })
-}
-
-
-addMask(currentAlive)
-
-// const mask = [
-//     [1,0], [],[],
-//     [],[0,0],[],
-//     [],[],[]
-// ]
