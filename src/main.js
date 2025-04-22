7c450d1e0b8a976e6038f7a447a211484dccea7c
Chris Annunziato <cmannunziato@gmail.com>
2025-04-22 17:44:05 -0400

    sorted the isAliveSet

--- src/main.js
+++ src/main.js
@@ -1,99 +1,104 @@
 import './style.css'
 
-const app = document.querySelector('#app')
+const app = document.querySelector ('#app')
 
 app.innerHTML = `
 <div class="h-screen w-screen flex justify-center items-start pt-[10%] bg-cyan-950">
 <div class="grid-container bg-gray-400"> </div>
 </div>
 `
-const grid = document.querySelector('.grid-container')
+const grid = document.querySelector ('.grid-container')
 // MODEL
 
-//model of cell state in an object
+// model of cell state in an object
 const dataModel = (rowIndex, cellIndex) => ({
     rowIndex: rowIndex, cellIndex: cellIndex, isAlive: false
 })
 
-//array in memory
-const arr = Array.from({length: 15}, (_, rowIndex) => Array.from({length: 40}, (_, cellIndex) => dataModel(rowIndex, cellIndex)))
+// array in memory
+const arr = Array.from (
+    {length: 15},
+    (_, rowIndex) => Array.from ({length: 40}, (_, cellIndex) => dataModel (rowIndex, cellIndex))
+)
 
-//VIEW
+// VIEW
 
-//IS ALIVE ARRAY
-const isAliveSet = new Set()
+// IS ALIVE ARRAY
+let isAliveSet = new Set ()
 
-//gen a random alive cell
-function generateAliveGridCell() {
-    const randRowInt = Math.floor(Math.random() * 15)
-    const randCellInt = Math.floor(Math.random() * 60)
+// gen a random alive cell
+function generateAliveGridCell () {
+    const randRowInt = Math.floor (Math.random () * 15)
+    const randCellInt = Math.floor (Math.random () * 60)
     const cellId = `${randRowInt}-${randCellInt}`
-    if (isAliveSet.has(cellId)) {
+    if (isAliveSet.has (cellId)) {
         if (isAliveSet.size < 100) {
-            return generateAliveGridCell()
+            return generateAliveGridCell ()
         } else return
     } else {
-        isAliveSet.add(cellId)
+        isAliveSet.add (cellId)
         return cellId
     }
 }
 
-//create divs, rows and cells
-//decorate them and append them to each other creating a 2d matrix
-arr.forEach((row, rowIdx) => {
-    const rowContainer = document.createElement('div')
+// create divs, rows and cells
+// decorate them and append them to each other creating a 2d matrix
+arr.forEach ((row, rowIdx) => {
+    const rowContainer = document.createElement ('div')
     rowContainer.className = (`row-container flex`)
 
-    grid.appendChild(rowContainer)
+    grid.appendChild (rowContainer)
 
-    row.forEach((cell, cellIdx) => {
-        const cellDiv = document.createElement('div')
+    row.forEach ((cell, cellIdx) => {
+        const cellDiv = document.createElement ('div')
         cellDiv.className = (`cell border h-4 w-4 ${cell.isAlive ? 'bg-green-500' : ''}`)
         cellDiv.dataset.id = `${rowIdx}-${cellIdx}`
         cellDiv.dataset.isAlive = cell.isAlive
 
-        rowContainer.appendChild(cellDiv)
+        rowContainer.appendChild (cellDiv)
     })
 })
 
-console.log(grid)
+// console.log(grid)
 
 
-function genBoard() {
+function genBoard () {
 
     for (let i = 0; i < 100; i++) {
-        //gen a valid row-int string literal
-        const randRowCellId = generateAliveGridCell()
+        // gen a valid row-int string literal
+        const randRowCellId = generateAliveGridCell ()
         if (randRowCellId === undefined) break
-        //grab a ref to the dom where: dataset.id === row-int string literal
-        //this is the model to view connection
-        const cell = document.querySelector(`[data-id='${randRowCellId}']`)
+        // grab a ref to the dom where: dataset.id === row-int string literal
+        // this is the model to view connection
+        const cell = document.querySelector (`[data-id='${randRowCellId}']`)
         if (!cell) continue
-        //set is alive
+        // set is alive
         cell.dataset.isAlive = 'true'
-        //change the bg
-        cell.classList.add(`${cell.dataset.isAlive ? 'bg-green-400' : ''}`)
+        // change the bg
+        cell.classList.add (`${cell.dataset.isAlive ? 'bg-green-400' : ''}`)
     }
 }
 
-genBoard()
+genBoard ()
 
-const transform = [
-    [-1, -1], [-1, 0], [-1, 1],
-    [0, -1], [0, 0], [0, 1],
-    [1, -1], [1, 0], [1, 1]
-]
+const transform = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
 
-// console.log(isAliveSet)
 
-//quicksort for fun
-function quicksort (iterable) {
-    const unsortedArr = [...iterable]
-    const sortedArr = []
-    const [row,col] = unsortedArr[0].split('-').map((str) => Number(str))
-
-    return sortedArr
+function sortRowColPairs (pairs) {
+    return [...pairs].sort ((a, b) => {
+        const [aRow, aCell] = a.split('-').map((value) => parseInt(value, 10))
+        const [bRow, bCell] = b.split('-').map((value) => parseInt(value, 10))
+        const rowDiff = aRow -bRow
+        if(rowDiff === 0) {
+            return aCell - bCell
+        }
+        return rowDiff
+    })
 }
 
-// console.log([...isAliveSet])
-console.log(quicksort(isAliveSet), 'quicksort')
+isAliveSet = new Set (sortRowColPairs(isAliveSet))
+console.log (isAliveSet)
+
+
+
+
